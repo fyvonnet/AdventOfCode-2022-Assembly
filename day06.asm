2c772afacb5cd4c89ac2411b7ae73296ef3e1c17
Franck YVONNET <franck@yvonnet.fr>
2023-01-09 08:24:43 +0100

    part 2

--- day06.asm
+++ day06.asm
@@ -8,12 +8,16 @@ extern malloc
 section .data
 
     filename    db  "inputs/day06",0
+    array       times 26 db 0
+    nuniques    db 0
     SYS_READ    equ  0
     SYS_WRITE   equ  1
     SYS_CLOSE   equ  3   
     SYS_EXIT    equ 60
     ASCII_A     equ 97
     ASCII_NL    equ 10
+    ADD_CHAR    equ  1
+    REM_CHAR    equ -1
 
 section .bss
 
@@ -42,6 +46,10 @@ loop_read:
     jmp         loop_read
 end_read:
 
+    mov         rax, SYS_CLOSE
+    mov         rdi, [fd]
+    syscall
+
     mov         rdi, r8
     push        r8
     call        malloc
@@ -58,11 +66,11 @@ loop_fill:
     dec         r9
     loop        loop_fill
 
-    mov         r8, 0
+; PART 1
+
     mov         r9, [input]
     dec         r9
-loop:
-    inc         r8
+loop_part1:
     inc         r9
     mov         r10b, byte [r9 + 0]
     mov         r11b, byte [r9 + 1]
@@ -70,24 +78,76 @@ loop:
     mov         r13b, byte [r9 + 3]
 
     cmp         r10b, r11b
-    je          loop
+    je          loop_part1
     cmp         r10b, r12b
-    je          loop
+    je          loop_part1
     cmp         r10b, r13b
-    je          loop
+    je          loop_part1
     cmp         r11b, r12b
-    je          loop
+    je          loop_part1
     cmp         r11b, r13b
-    je          loop
+    je          loop_part1
     cmp         r12b, r13b
-    je          loop
+    je          loop_part1
     
-    add         r8, 3 ; end of packet *after* the marker
+    mov         rdi, r9
+    sub         rdi, [input]
+    add         rdi, 4 ; end of packet *after* the marker
+    call        print_int
+
+
+; PART 2
+
+    mov         rcx, 14
+    mov         r8, [input]
+loop_fill_array:
+    mov         rdi, r8
+    mov         rsi, ADD_CHAR
+    call        update_array
+    inc         r8
+    loop        loop_fill_array
+
+    mov         r8, [input]
+loop_part2:
+    cmp         byte [nuniques], 14
+    je          end_part2
+    mov         rdi, r8
+    mov         rsi, REM_CHAR
+    call        update_array
+    mov         rdi, r8
+    add         rdi, 14
+    mov         rsi, ADD_CHAR
+    call        update_array
+    inc         r8
+    jmp         loop_part2
+
+end_part2:
 
     mov         rdi, r8
+    sub         rdi, [input]
+    add         rdi, 14
     call        print_int
 
     mov         rax, SYS_EXIT
     mov         rdi, 0
     syscall
 
+update_array:
+    push        r8
+    xor         r8, r8
+    mov         r8b, byte [rdi]
+    lea         r8, [array + r8]
+    xor         rax, rax
+    mov         al, byte [r8]
+    cmp         rax, 1
+    jne         skip_rem
+    dec         byte [nuniques]
+skip_rem:
+    add         rax, rsi
+    cmp         rax, 1
+    jne         skip_add
+    inc         byte [nuniques]
+skip_add:
+    mov         byte [r8], al
+    pop         r8
+    ret
